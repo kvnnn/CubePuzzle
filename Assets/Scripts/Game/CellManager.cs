bcae326bf1b4e4f64d5c8cb8e0965309f6e8e4bf
Kevin Saito <yum02.skullcandy@gmail.com>
2013-12-30 02:28:32 +0900

    add cell

---
+++ Assets/Scripts/Game/CellManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CellManager : SingletonMonoBehaviour<CellManager>
+{
+// Scene
+	public Game game;
+
+
+
+}
--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class Game : SingletonMonoBehaviour<Game>
 {
 // Scene
+	public CellManager cellManager;
 
 // NGUI
 	public GameObject nguiGame;
