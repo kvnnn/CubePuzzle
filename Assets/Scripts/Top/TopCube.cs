ce76c884f2d2a57904f607a157bfb4e14c653dfe
Kevin Saito <yum02.skullcandy@gmail.com>
2013-12-29 18:12:06 +0900

    add mat

--- Assets/Scripts/Top/TopCube.cs
+++ Assets/Scripts/Top/TopCube.cs
@@ -4,20 +4,16 @@ using System.Collections.Generic;
 
 public class TopCube : MonoInheritance
 {
-
 	public void OnShow()
 	{
-
-	}
-
-	private void GradientColor()
-	{
-
+		RotateRandomly();
 	}
 
 	private void RotateRandomly()
 	{
-
+		TweenRotation _rotate = TweenRotation.Begin(gameObject, Random.Range(3f, 4f), Quaternion.Euler(new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360))));
+		_rotate.onFinished = (_tween)=>{
+			RotateRandomly();
+		};
 	}
-
 }
